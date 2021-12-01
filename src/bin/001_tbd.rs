4c3c136419820acba86d37a5440f1d9c5ff99ce2
Ioan Andrei Bârsan <andrei.ioan.barsan@gmail.com>
2021-12-01 00:19:32 -0500

    AoC 2021 Day 1 finished

---
+++ src/bin/001_tbd.rs
@@ -0,0 +1,58 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use std::str::FromStr;
+use std::collections::HashMap;
+
+
+fn main() {
+    // Note: This path is relative to the repo root
+    let input_path = Path::new("input/001.txt");
+
+    let mut prev: i32 = 99999999;
+    let mut inc: i32 = 0;
+    let mut inc_sum: i32 = 0;
+
+    let mut nrs: i32 = 0;
+    let mut q: [i32; 3] = [0i32, 0i32, 0i32];
+    let mut prev_sum: i32= 0;
+
+    if let Ok(lines) = read_lines(input_path) {
+        for maybe_line in lines {
+            if let Ok(line) = maybe_line {
+                // Part 1 of the problem
+                let cur = i32::from_str(&line).unwrap();
+                if cur > prev {
+                    inc += 1;
+                }
+                prev = cur;
+
+                // Part 2 of the problem
+                for ii in 0..2 {
+                    q[ii] = q[ii + 1];
+                }
+                q[2] = cur;
+                let cur_sum = q.iter().sum();
+
+                nrs += 1;
+                if nrs >= 4 {
+                    if cur_sum > prev_sum {
+                        inc_sum += 1;
+                    }
+
+                }
+                prev_sum = cur_sum;
+            }
+        }
+    }
+
+    println!("Final result {}", inc);
+    println!("Final result {}", inc_sum);
+}
+
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
